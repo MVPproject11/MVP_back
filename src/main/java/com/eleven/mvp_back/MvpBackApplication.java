6ef9e9ebd36f8acc0c9e54588e173414a277ada3
ljg0610 <jggy0627@naver.com>
2025-02-10 13:43:26 +0900

    feat: init project

---
+++ src/main/java/com/eleven/mvp_back/MvpBackApplication.java
@@ -0,0 +1,13 @@
+package com.eleven.mvp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eleven/mvp_back/MvpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleven.mvp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
