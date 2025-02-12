4cbd34ae80a1fc73b22c4c9becc114acf671b5d8
ljg0610 <jggy0627@naver.com>
2025-02-12 16:38:37 +0900

    feat: created, nocontent 공통응답 추가

--- src/main/java/com/eleven/mvp_back/common/ApiResponse.java
+++ src/main/java/com/eleven/mvp_back/common/ApiResponse.java
@@ -15,6 +15,7 @@ public class ApiResponse<T> {
     private String message;
     private T data;
 
+    //200-OK
     public static <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
                 .code(HttpStatus.OK.value())
@@ -23,6 +24,7 @@ public class ApiResponse<T> {
                 .build();
     }
 
+    //200-OK
     public static <T> ApiResponse<T> success(String message, T data) {
         return ApiResponse.<T>builder()
                 .code(HttpStatus.OK.value())
@@ -31,6 +33,25 @@ public class ApiResponse<T> {
                 .build();
     }
 
+    //201-CREATED
+    public static <T> ApiResponse<T> created(String message, T data) {
+        return ApiResponse.<T>builder()
+                .code(HttpStatus.CREATED.value())
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    //204-NO CONTENT
+    public static <T> ApiResponse<T> noContent(String message) {
+        return ApiResponse.<T>builder()
+                .code(HttpStatus.NO_CONTENT.value())
+                .message(message)
+                .data(null)
+                .build();
+    }
+
+    //에러
     public static <T> ApiResponse<T> error(int code, String message) {
         return ApiResponse.<T>builder()
                 .code(code)
