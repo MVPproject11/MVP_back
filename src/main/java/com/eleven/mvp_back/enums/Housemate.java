fab7616f63568e78f2ac41effcbcd8f0f161d1d1
ljg0610 <jggy0627@naver.com>
2025-02-13 15:00:18 +0900

    feat: 어르신 엔티티 추가

---
+++ src/main/java/com/eleven/mvp_back/enums/Housemate.java
@@ -0,0 +1,19 @@
+package com.eleven.mvp_back.enums;
+
+public enum Housemate {
+    LIVING_ALONE("독거"),
+    WITH_SPOUSE_AT_HOME("배우자와 동거, 돌봄 시간 중 집에 있음"),
+    WITH_SPOUSE_AWAY("배우자와 동거, 돌봄 시간 중 자리 비움"),
+    WITH_FAMILY_AT_HOME("다른 가족과 동거, 돌봄 시간 중 집에 있음"),
+    WITH_FAMILY_AWAY("다른 가족과 동거, 돌봄 시간 중 자리 비움");
+
+    private final String displayName;
+
+    Housemate(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
---
+++ src/main/java/com/eleven/mvp_back/enums/SymptomsDementia.java
@@ -0,0 +1,22 @@
+package com.eleven.mvp_back.enums;
+
+public enum SymptomsDementia {
+    NORMAL("정상 (치매 증상 없음)"),
+    WANDERING_OUTSIDE("집 밖을 배회"),
+    SHORT_TERM_MEMORY_IMPAIRMENT("했던 말을 반복하는 등 단기 기억 장애"),
+    NOT_RECOGNIZING_FAMILY("가족을 알아보지 못함"),
+    GETTING_LOST("길을 잃거나 자주 가던 곳을 헤맴"),
+    PARANOID_DELUSIONS("사람을 의심하는 망상"),
+    CHILDLIKE_BEHAVIOR("어린아이 같은 행동"),
+    AGGRESSIVE_BEHAVIOR("때리거나 욕설 등 공격적인 행동");
+
+    private final String displayName;
+
+    SymptomsDementia(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
