02330ed55a0956cb1f218c69a34efb5a14690a67
ljg0610 <jggy0627@naver.com>
2025-02-15 01:25:18 +0900

    feat: 회원가입 dto 추가

---
+++ src/main/java/com/eleven/mvp_back/domain/dto/request/SignupRequest.java
@@ -0,0 +1,19 @@
+package com.eleven.mvp_back.domain.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이어야 합니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수입니다,")
+        @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하로 입력해주세요.")
+        String password,
+
+        @NotBlank(message = "직무선택은 필수입니다.")
+        String role
+) {
+}
