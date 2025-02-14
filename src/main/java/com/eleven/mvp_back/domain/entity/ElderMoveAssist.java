f89ba11dffdafa469137f854f94c220840b6f2e9
ljg0610 <jggy0627@naver.com>
2025-02-14 19:17:04 +0900

    feat: 이동보조 엔티티 추가

---
+++ src/main/java/com/eleven/mvp_back/domain/entity/ElderMoveAssist.java
@@ -0,0 +1,25 @@
+package com.eleven.mvp_back.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Table(name = "elder_move_assists")
+public class ElderMoveAssist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "elder_move_assist_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "elder_id", nullable = false)
+    private Elder elder;
+
+    @Column(nullable = false, length = 50)
+    private String moveServiceName;
+}
