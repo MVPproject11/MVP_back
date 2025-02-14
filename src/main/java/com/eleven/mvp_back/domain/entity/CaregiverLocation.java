9faa9d66d0878ecb94345904cba089ee53326929
ljg0610 <jggy0627@naver.com>
2025-02-14 16:49:21 +0900

    feat: 근무가능지역 엔티티 추가

--- src/main/java/com/eleven/mvp_back/domain/entity/CaregiverLocation.java
+++ src/main/java/com/eleven/mvp_back/domain/entity/CaregiverLocation.java
@@ -1,4 +1,33 @@
 package com.eleven.mvp_back.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Table(name = "caregiver_locations")
 public class CaregiverLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "caregiver_location_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "caregiver_id", nullable = false)
+    private Caregiver caregiver;
+
+    @Column(nullable = false, length = 20)
+    private String city;
+
+    @Column(nullable = false, length = 10)
+    private String district;
+
+    @Column(nullable = false, length = 10)
+    private String dong;
 }
