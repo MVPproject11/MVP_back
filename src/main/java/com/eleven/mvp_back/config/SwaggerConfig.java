f95a5438c4af51c34021eb2051a14d8d88c9eef2
ljg0610 <jggy0627@naver.com>
2025-02-15 12:36:21 +0900

    feat: 로그인 요청 dto 추가

--- src/main/java/com/eleven/mvp_back/config/SwaggerConfig.java
+++ src/main/java/com/eleven/mvp_back/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8081").description("Local API Server"))
                 .components(new Components()
                         .addResponses("200", new ApiResponse().description("Ok"))
                         .addResponses("201", new ApiResponse().description("Created"))
