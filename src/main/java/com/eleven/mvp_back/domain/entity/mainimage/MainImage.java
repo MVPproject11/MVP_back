579c7c12e3ad64c71a26d387c295353a68a5a677
ljg0610 <jggy0627@naver.com>
2025-02-20 02:28:03 +0900

    feat: 메인 전시용 이미지 엔티티 추가

---
+++ src/main/java/com/eleven/mvp_back/domain/entity/mainimage/MainImage.java
@@ -0,0 +1,21 @@
+package com.eleven.mvp_back.domain.entity.mainimage;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "main_images")
+public class MainImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "main_image_id")
+    private Long id;
+
+
+    private String mainPhoto;
+}
